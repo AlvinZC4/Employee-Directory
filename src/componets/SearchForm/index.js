c1787cb5a570cc4a5ba7a451ea74df3458a35e0a
Alvin <alvinzcoxiv@gmail.com>
2020-10-07 17:46:09 -0400

    Cleaned up search bar, still not final

--- src/componets/SearchForm/index.js
+++ src/componets/SearchForm/index.js
@@ -2,29 +2,23 @@ import React from "react"
 
 function SearchForm(props) {
     return (
-        <form className="search">
-          <div className="form-group">
-            <label htmlFor="name">Search by name:</label>
-            <input
-              value={props.search}
-              onChange={props.handleInputChange}
-              name="name"
-              list="names"
-              type="text"
-              className="form-control"
-              placeholder="Search Employees by name"
-              id="name"
-            />
-            {/* <datalist id="names">
-              {props.names.map(name => (
-                <option value={name} key={name} />
-              ))}
-            </datalist> */}
-            <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-              Search
-            </button>
+      <form className="search">
+        <div className="form-group row col-md-6">
+          <label htmlFor="name" className="col-form-label col-md-4">Search by name</label>
+          <div className="col-md-8">
+          <input
+            type="email"
+            value={props.search}
+            onChange={props.handleInputChange}
+            name="name"
+            list="names"
+            className="form-control"
+            placeholder="Search Employees Here"
+            id="inputEmail4"
+          />
           </div>
-        </form>
+        </div>
+      </form>
     );
 }
 
