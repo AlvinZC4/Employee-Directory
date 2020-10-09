b067a07812aff9d014b659bb99f0e3defd9a4b6e
Alvin <alvinzcoxiv@gmail.com>
2020-10-08 22:49:55 -0400

    updated table, searchForm, searchResults, & image to properly take props. created main.js

--- src/componets/Table/index.js
+++ src/componets/Table/index.js
@@ -1,37 +1,19 @@
 import React from "react"
+import SearchResults from "../SearchResults"
 
 function Table(props) {
     return(
         <table className="table">
-  <thead className="thead-dark">
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">E-mail</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>mark@mdo.com</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>toto@fat.net</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>Gurado@twittercom</td>
-    </tr>
-  </tbody>
-</table>
+            <thead className="thead-dark">
+                <tr>
+                <th scope="col"></th>
+                <th scope="col">{props.firstHeader}</th>
+                <th scope="col">{props.lastHeader}</th>
+                <th scope="col">{props.emailHeader}</th>
+                </tr>
+            </thead>
+            <SearchResults results={props.results} />
+        </table>
     )
 }
 
---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import React, {Component, useState, useEffect} from "react"
+import SearchForm from "../componets/SearchForm"
+import SearchResults from "../componets/SearchResults"
+
+function Main() {
+    const [employee, setEmployee] = useState({})
+    const [employees, setEmployees] = useState([])
+    const [employeeIndex, setEmployeeIndex] = useState(0)
+    
+}
