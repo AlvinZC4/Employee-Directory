09ef74a422a576dcda53c6a89d9bd9ecc2ee9324
Alvin <alvinzcoxiv@gmail.com>
2020-10-14 21:31:35 -0400

    removed React.router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom"
 import NavBar from "./componets/NavBar"
 import Main from "./pages/Main"
 import Wrapper from "./componets/Wrapper"
 
 function App() {
   return (
-    <Router>
       <div>
         <NavBar />
         <Wrapper>
           <Main />
         </Wrapper>
       </div>
-    </Router>
   );
 }
 
